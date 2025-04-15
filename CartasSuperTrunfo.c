2903306991af22adc6c1800a5828586452485c06
ALINEQDS <89817138+ALINEQDS@users.noreply.github.com>
2025-04-15 09:27:13 -0300

    Update CartasSuperTrunfo.c
    Desafio novato - SuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,56 @@
 #include <stdio.h>
 
+int main() {
+
+    char estado[30], estado2[30];
+    char codigocarta[4], codigocartas02[4];
+    char cidade[50], cidade02[50];
+    int pontosturisticos, pontosturisticos02;
+    float populacao, populacao02; 
+    float pib, pib02;
+    float area, area02;
+    
+    // DADOS DA PRIMEIRA CARTA //
+
+    printf("ESTADO: ");
+    scanf("%s", estado);
+
+    printf("CIDADE: ");
+    scanf("%s", cidade);
+
+    printf("CODIGO DE CARTAS: ");
+    scanf("%s", codigocarta);
+
+    printf("PONTOS TURISTICOS: ");
+    scanf("%d", &pontosturisticos);
+
+    // DADOS DA SEGUNDA CARTA 02 //
+
+    printf("ESTADO_02:");
+
+    scanf("%s", estado2);
+
+    printf("CIDADE: ");
+    scanf("%s", cidade02);
+
+    printf("CODIGO DE CARTAS: ");
+    scanf("%s", codigocartas02);
+
+    printf("PONTOS TURISTICOS: ");
+    scanf("%d", &pontosturisticos02);
+
+    printf("POPULACAO: ");
+    scanf("%f", &populacao02);
+
+    printf("PIB: ");
+    scanf("%f", &pib02);
+
+    printf("AREA EM KM²: ");
+    scanf("%f", &area02);
+
+    return 0;
+}#include <stdio.h>
+
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
